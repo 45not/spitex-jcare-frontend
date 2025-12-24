8a484f8bab5e25a351c80075b377fe3690eadad4
Sam Elevitsky <elevitsky.sam@gmail.com>
2025-12-24 09:21:34 +0100

    Remove reCAPTCHA remnants and unused dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import "./i18n/i18n";
 
 // Debug environment variables
 //console.log('Environment:', import.meta.env.MODE);
-//console.log('Site Key:', import.meta.env.VITE_RECAPTCHA_SITE_KEY);
+
 //console.log('All env vars:', import.meta.env);
 
 
